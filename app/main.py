81600f52b9dc4f8e443e5b1f4831d99251d05add
Anvesh Shrivastava <itsofficialanvesh@gmail.com>
2024-06-02 14:58:58 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def main():
         sys.stdout.write("$ ")  # maybe we are using this to write instead of print as that appends and extra newline
         sys.stdout.flush()
 
-        list_of_commands = ['exit']
+        list_of_commands = ['exit', 'echo']
         statement = input()
 
         command = statement.split()[0]
@@ -17,6 +17,9 @@ def main():
             continue
         if command == 'exit':
             break
+        if command == 'echo':
+            print(''.join(statement.split()[1:]))
+            continue
 
 if __name__ == "__main__":
     main()
