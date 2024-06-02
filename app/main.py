e67c4617110d946018549b8f7888f33bb10c9764
Anvesh Shrivastava <itsofficialanvesh@gmail.com>
2024-06-02 14:35:23 +0530

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -3,13 +3,10 @@ import sys
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
 
-    # Uncomment this block to pass the first stage
-    # sys.stdout.write("$ ")
-    # sys.stdout.flush()
+    sys.stdout.write("$ ") # maybe we are using this to write instead of print as that appends and extra newline
+    sys.stdout.flush()
 
-    # Wait for user input
     input()
 
 
