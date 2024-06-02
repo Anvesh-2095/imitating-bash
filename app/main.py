24ce9e3108220b593d204939d6edab625c7e12fc
Anvesh Shrivastava <itsofficialanvesh@gmail.com>
2024-06-02 14:47:08 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -4,15 +4,15 @@ import sys
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
 
-    sys.stdout.write("$ ") # maybe we are using this to write instead of print as that appends and extra newline
-    sys.stdout.flush()
+    while 1:
+        sys.stdout.write("$ ")  # maybe we are using this to write instead of print as that appends and extra newline
+        sys.stdout.flush()
 
-    list_of_commands = []
-    command = input()
+        list_of_commands = []
+        command = input()
 
-    if command not in list_of_commands:
-        print(command + ": command not found")
-        exit(1)
+        if command not in list_of_commands:
+            print(command + ": command not found")
 
 if __name__ == "__main__":
     main()
