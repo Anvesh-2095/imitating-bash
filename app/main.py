2a8f7d410aeb5680fd822be0f72ca458206b36a5
Anvesh Shrivastava <itsofficialanvesh@gmail.com>
2024-06-02 14:44:23 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -7,8 +7,12 @@ def main():
     sys.stdout.write("$ ") # maybe we are using this to write instead of print as that appends and extra newline
     sys.stdout.flush()
 
-    input()
+    list_of_commands = []
+    command = input()
 
+    if command not in list_of_commands:
+        print(command + ": command not found")
+        exit(1)
 
 if __name__ == "__main__":
     main()
